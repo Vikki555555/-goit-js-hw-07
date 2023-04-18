da1cfdbc31471a2ec34c228e264bdfd8ded995df
Vikki555555 <130974737+Vikki555555@users.noreply.github.com>
2023-04-18 14:13:32 +0300

    tasks10
    tasks10

--- js/task-10.js
+++ js/task-10.js
@@ -1,17 +1,25 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+//function getRandomHexColor() {
+ // return `#${Math.floor(Math.random() * 16777215)
+    //.toString(16)
+    //.padStart(6, 0)}`;
+//}
 const controls = document.querySelector('#controls');
 const boxes = document.querySelector('#boxes');
+
+controls.addEventListener('click', (e) => {
+  if (e.target.dataset.create) {
+    createBoxes(+controls.firstElementChild.value);
+  } else if (e.target.dataset.destroy) {
+    destroyBoxes();
+  }
+});
 
 function createBoxes(amount) {
   for (let i = 0; i < amount; i++) {
     const box = document.createElement('div');
 
-    box.style.width = `${30 + 10 * i}px`;
-    box.style.height = `${30 + 10 * i}px`;
+    box.style.width = 30 + 10 * i + 'px';
+    box.style.height = 30 + 10 * i + 'px';
     box.style.backgroundColor = getRandomHexColor();
 
     boxes.appendChild(box);
@@ -22,10 +30,8 @@ function destroyBoxes() {
   boxes.innerHTML = '';
 }
 
-controls.addEventListener('click', (e) => {
-  if (e.target.dataset.create) {
-    createBoxes(+controls.querySelector('input').value);
-  } else if (e.target.dataset.destroy) {
-    destroyBoxes();
-  }
-});
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
