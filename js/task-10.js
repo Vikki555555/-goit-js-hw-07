f4248dc91ab4fb3cb1fd11ebb59925cc32a1cc77
Vikki555555 <130974737+Vikki555555@users.noreply.github.com>
2023-04-18 13:35:40 +0300

    to do tasks
    to do tasks 3,4

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,29 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const controls = document.querySelector('#controls');
+const boxes = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+
+    box.style.width = `${30 + 10 * i}px`;
+    box.style.height = `${30 + 10 * i}px`;
+    box.style.backgroundColor = getRandomHexColor();
+
+    boxes.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+
+controls.addEventListener('click', (e) => {
+  if (e.target.dataset.create) {
+    createBoxes(+controls.querySelector('input').value);
+  } else if (e.target.dataset.destroy) {
+    destroyBoxes();
+  }
+});
