92e7506aa414218d85515f8fff161506712e6d44
Vikki555555 <130974737+Vikki555555@users.noreply.github.com>
2023-04-24 13:01:22 +0300

    Update task-07.js
    task7

---
+++ js/task-07.js
@@ -0,0 +1,4 @@
+const input=document.querySelector('#font-size-control');
+const text = document.querySelector('#text');
+fontSizeControl.addEventListener('input', () => {  text.style.fontSize = fontSizeControl.value + 'px';
+});
