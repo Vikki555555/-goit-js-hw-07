d83bc9535da69db36356db4f6ceb198cf1be1297
Vikki555555 <130974737+Vikki555555@users.noreply.github.com>
2023-04-24 15:46:04 +0300

    Update task-02.js
    update2

--- js/task-02.js
+++ js/task-02.js
@@ -7,11 +7,11 @@ const ingredients = [
   'Condiments',
 ];
 const list = document.querySelector('#ingredients');
-const array=[]
+const array=[];
 for (let i = 0; i < ingredients.length; i++) {
   const item = document.createElement('li');
   item.className = 'item';
   item.textContent = ingredients[i];
- array.push(item)
-}
+ array.push(item);
+};
 list.append(...array);
