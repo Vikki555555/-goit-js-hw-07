59c6028028dc496c2e1b2df97687b12684f75f98
Vikki555555 <130974737+Vikki555555@users.noreply.github.com>
2023-04-24 13:17:09 +0300

    Update task-08.js
    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+const formData = {};
+  for (let i = 0; i < form.elements.length; i += 1) {
+    const input = form.elements[i];
+    if (input.name) {
+      formData[input.name] = input.value;
+    }
+  }
+
+  if (Object.values(formData).some(value => value === '')) {
+    alert('All fields must be filled!');
+  } else {
+    console.log(formData);
+    form.reset();
+  }
+});
